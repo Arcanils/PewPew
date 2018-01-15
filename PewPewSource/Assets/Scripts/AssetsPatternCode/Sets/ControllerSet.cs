be7941918c42b978e94b7e61aaf9286a4dc88c08
Petiot thibaut <petiotthib@gmail.com>
2018-01-15 14:54:45 +0100

    Config controller in progress RuntimeSet in progress Roadmap update

---
+++ PewPewSource/Assets/Scripts/AssetsPatternCode/Sets/ControllerSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AssetsPattern
+{
+	[CreateAssetMenu]
+	public class ControllerSet : RuntimeSet<BaseController>
+	{
+
+	}
+}
---
+++ PewPewSource/Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AssetsPattern;
+
+public class GameCamera : MonoBehaviour
+{
+	public ControllerSet ListEntityToWatch;
+	public float OffsetY;
+
+
+	private Transform _transCam;
+
+	public void Awake()
+	{
+		_transCam = transform;
+	}
+
+	public void Reset()
+	{
+		OffsetY = 4;
+	}
+
+	public void UpdatePosCam()
+	{
+		float Center = 0f;
+		if (ListEntityToWatch != null && ListEntityToWatch.Items.Count > 0)
+		{
+
+			for (int i = ListEntityToWatch.Items.Count - 1; i >= 0; --i)
+			{
+				if (ListEntityToWatch.Items[i] != null)
+					Center += ListEntityToWatch.Items[i].PawnPosition.y;
+			}
+			
+			Center = Mathf.Clamp(Center / ListEntityToWatch.Items.Count, -OffsetY, OffsetY);
+		}
+
+		_transCam.position = new Vector3(_transCam.position.x, Center, _transCam.position.z);
+	}
+}
